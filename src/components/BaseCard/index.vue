fe1429e5271805306d51ecdb53604363838919cc
BerdiyevAbror <islamiskiyaxi@gmail.com>
2023-02-13 21:32:53 +0300

    chore: teacher page created

---
+++ src/components/BaseCard/index.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import BaseIcon from "@/components/BaseIcon/index.vue";
+const props = defineProps({
+  cardData: {
+    type: Object,
+    default: {},
+    required: true,
+  },
+});
+</script>
+
+
+<template >
+  <div>
+    <div
+      class="
+      
+        wrapper
+        px-14
+        bg-white
+        rounded-2xl
+        gap-1
+        text-black
+        pt-5
+        pb-8
+        shadow-md
+      "
+    >
+      <img src="/images/user.png" alt="" class="mx-auto" />
+      <h1 class="text-center text-3xl text-[#333333]">Shermat xolmatov</h1>
+      <p class="text-center text-base text-[#706B6B]">Teacher</p>
+      <p
+        class="number text-center text-base text-[#706B6B] mt-4 cursor-pointer flex justify-center gap-0.5"
+      >
+      <BaseIcon class="w-5 h-5 text-prmary-900" name="Telephone"/>
+        +998990651701
+      </p>
+      <p class="text-center text-[15px] text-[#706B6B] cursor-pointer flex justify-center gap-0.5 ">
+        <BaseIcon class="w-5 h-5 text-prmary-900" name="Email"/>
+         info@onic.desing
+      </p>
+    </div>
+  </div>
+</template>
+  
--- src/views/Teacher/index.vue
+++ src/views/Teacher/index.vue
@@ -1,3 +1,24 @@
-<template>
-    this is teacher page
+<script setup lang="ts">
+import BaseCard from "@/components/BaseCard/index.vue";
+
+</script>
+
+
+<template >
+  <div class="">
+    <h1
+      class="
+        text-3xl text-[#5F5F5F]
+        font-semibold
+        mt-18
+        ml-6
+        mb-12 
+      "
+    >
+      Responsible staff and teachers
+    </h1>
+    <div class="grid grid-cols-3 gap-12">
+        <BaseCard v-for="i in 18" :key="i" class="" />
+    </div>
+  </div>
 </template>
